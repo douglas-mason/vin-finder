838db59d485d3e8449d61676ab042d79c9e585b1
Doug Mason <doug@parsed.io>
2019-08-07 00:15:19 -0400

    updates

--- src/landing/header/header.component.tsx
+++ src/landing/header/header.component.tsx
@@ -3,5 +3,5 @@ import { jsx } from '@emotion/core';
 import { containerClass } from './header.styles';
 
 export const Header = () => {
-  return <section css={containerClass}>Vin Finder</section>;
+  return <section css={containerClass}>VIN Finder</section>;
 };
--- src/landing/header/header.styles.ts
+++ src/landing/header/header.styles.ts
@@ -3,5 +3,4 @@ import { css } from '@emotion/core';
 export const containerClass = css({
   margin: 20,
   fontSize: '44px',
-  textTransform: 'capitalize',
 });
